ba58f159cbbff24251e0239cce08c706b0a85d0e
Herman <s32370@pjwstk.edu.pl>
2025-03-14 11:13:31 +0100

    TASK 1; still contains mistake

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,34 @@
-//github test
+import java.util.Arrays;
+
+public class Main {
+    public static int[] filterEvenIndexOddValue(int[] array) {
+
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0 && array[i] % 2 != 0) {
+                result[index] = array[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] testArray1 = {7,8,9,10,11};
+
+        int[] filterArray1 = filterEvenIndexOddValue(testArray1);
+
+        System.out.println(Arrays.toString(filterArray1));
+          for (int num: (filterArray1)) {
+           System.out.print(num + " ");
+          }
+    }
+}
